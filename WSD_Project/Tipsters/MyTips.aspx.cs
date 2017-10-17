b59a4b2c034442607a3d18a36c20b08b4fa136cc
Kristopher <kris_byrnes@hotmail.com>
2017-10-17 21:04:58 +1100

    MyTips update

--- WSD_Project/Tipsters/MyTips.aspx.cs
+++ WSD_Project/Tipsters/MyTips.aspx.cs
@@ -15,7 +15,28 @@ namespace WSD_Project.Tipsters
         protected void Page_Load(object sender, EventArgs e)
         {
             SqlDataSource1.SelectParameters["username"].DefaultValue = Page.User.Identity.Name; // Finding username of user to check rounds
-        } 
-    }
 
+        }
+
+        protected void boxChange(object sender, EventArgs e)
+        {
+            if (DropDownList2.Items.Count == 0)
+                {
+                    DropDownList2.Visible = false;
+                Label1.Visible = true;
+
+                }
+                else
+                {
+                    DropDownList2.Visible = true;
+                Label1.Visible = false;
+
+                }
+            
+
+        }
+
+      
+    }
+    
 }
