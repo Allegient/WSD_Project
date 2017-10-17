6e9983d74f75473bc7aa2e1a9320ed3e60d07dbf
Christopher Narayan <18027697@student.westernsydney.edu.au>
2017-10-17 19:41:35 +1100

    7.1 done

--- WSD_Project/Administrators/ModifyTips.aspx.designer.cs
+++ WSD_Project/Administrators/ModifyTips.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace WSD_Project.Administrators {
     public partial class ModifyTips {
         
         /// <summary>
+        /// UpdatePanel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
         /// ListView1 control.
         /// </summary>
         /// <remarks>
